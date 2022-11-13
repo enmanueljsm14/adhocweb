286c8759c21f7456a7abe0a581f8fc3827c88d46
enmanueljsm <enmanueljsm@gmail.com>
2022-11-13 16:04:32 -0500

    initial

---
+++ adhocwep/adhocwep/db.py
@@ -0,0 +1,15 @@
+﻿import os
+
+BASE_DIR= os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+POSTGRESQL = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'webadhoc',
+        'USER': 'webadhoc',
+        'PASSWORD': 'webadhoc',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
--- adhocwep/adhocwep/settings.py
+++ adhocwep/adhocwep/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import adhocwep.db as db
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,12 +75,7 @@ WSGI_APPLICATION = 'adhocwep.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = db.POSTGRESQL
 
 
 # Password validation
@@ -118,6 +115,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles/")
+
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,"static/"),
+]
+
+
+MEDIA_URL="/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR,"media/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
